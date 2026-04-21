2baead5a904236a26f8ee94a6d62c369e3373b76
karim <karimmashaly9@gmail.com>
2026-04-21 13:50:01 +0200

    fix host binding

--- Masar Backend v1/Program.cs
+++ Masar Backend v1/Program.cs
@@ -36,8 +36,8 @@ namespace Masar_Backend_v1
 
 
             app.MapControllers();
-
-            app.Run();
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            app.Run($"http://0.0.0.0:{port}");
         }
     }
 }
