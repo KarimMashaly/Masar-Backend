0368eae06106c7dad60b2632aea14dfcc467df77
karim <karimmashaly9@gmail.com>
2026-04-24 23:28:48 +0300

    app.Run

--- Masar Backend v1/Program.cs
+++ Masar Backend v1/Program.cs
@@ -38,9 +38,8 @@ namespace Masar_Backend_v1
 
 
             app.MapControllers();
-            //var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            //app.Run($"http://0.0.0.0:{port}");
-            app.Run();
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            app.Run($"http://0.0.0.0:{port}");
         }
     }
 }
